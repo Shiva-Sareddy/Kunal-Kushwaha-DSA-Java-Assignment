21fcbd66743bcda606156a93220720b18a48bdbb
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-09-11 19:38:40 +0530

    second commit

--- 02-first-java/KunalDSA/K8.java
+++ 02-first-java/KunalDSA/K8.java
@@ -8,7 +8,7 @@ public class K8 {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a word to Know Whether it is Palindrome or not: ");
 		String word = sc.nextLine();
-		String sWord = word.replaceAll("\\s+", "").toLowerCase();
+		String sWord = word.replaceAll("\\s+", "").toLowerCase(); //Note: "\\s+" represents: "\\s" represents any White space character such as Space, newline, tab, carriage return, form feed. "+" indicates two or more spaces.
 		String revWord = "";
 		for(int i= sWord.length()-1; i >= 0; i--) {
 			revWord = revWord + sWord.charAt(i);
