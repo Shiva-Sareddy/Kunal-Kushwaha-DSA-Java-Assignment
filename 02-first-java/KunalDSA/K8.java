2be334a46fece703c29d36a4cd827969d37cdfe6
sareddy-shiva <153980370+sareddy-shiva@users.noreply.github.com>
2024-09-11 09:19:52 +0530

    First commit

---
+++ 02-first-java/KunalDSA/K8.java
@@ -0,0 +1,24 @@
+package KunalDSA;
+
+import java.util.Scanner;
+
+public class K8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a word to Know Whether it is Palindrome or not: ");
+		String word = sc.nextLine();
+		String sWord = word.replaceAll("\\s+", "").toLowerCase();
+		String revWord = "";
+		for(int i= sWord.length()-1; i >= 0; i--) {
+			revWord = revWord + sWord.charAt(i);
+		}
+		if (revWord.equals(sWord)) {
+			System.out.println("Palimdrone");
+		}else {
+			System.out.println("Not a Palindrome");
+		}
+		sc.close();
+	}
+
+}
